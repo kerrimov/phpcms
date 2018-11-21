f12d61b33406bb90397b8e1fe53c0782eb9be9e8
kerrimov <tolik.kerimov@gmail.com>
2018-11-21 14:12:41 +0200

    add contact form

---
+++ contact.php
@@ -0,0 +1,53 @@
+<?php 
+	include("functions/dbconnect.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Contact</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">-->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+</head>
+<body>
+	<div class="container">
+		<div class ="head">
+			<img src="img/header.jpg" id="logo">
+		</div>
+		
+		<div class="barmenu">
+			<ul id="menu">
+				<?php include ("includes/nav.php") ?>	
+			</ul>
+
+			<div id="form">
+				<form method="get" action="result.php" enctype="multipart/form-data">
+				<input style="border-radius: 10px;"type="text" name="searchquery">
+				<input style="border-radius: 10px; background-color: #e8e8e8;" type="submit" name="search" value="Search">
+			</form>
+			</div>	
+		</div>	
+
+		<div class="postarea">
+			<div class="sta" style="background-color: #ffffff">
+					<p style="font-family: MV Boli; font-size: 30px">For cooperation issues:</p>
+					<li style="font-family: MV Boli; font-size: 25px">Email: ✉ phpcms@gmail.com</li>
+					<li style="font-family: MV Boli; font-size: 25px">Phone: ☎ +380660770777</li>
+					<p style="font-family: MV Boli; font-size: 25px">Our office:<br> Phpcms street, 7 </p>
+					<img src="img/map.png" id="map">
+				
+			</div>			
+		</div>
+
+		<div class="sbar">
+			<?php include ("includes/sitebar.php")?>
+		</div>
+
+		<div class="foterarea">
+			<?php include ("includes/footer.php")?>
+		</div>
+	</div>
+	
+</body>
+</html>
