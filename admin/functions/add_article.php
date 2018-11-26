a4932fcdec55ae6be0e215efa724e8cbeb8acdc2
kerrimov <tolik.kerimov@gmail.com>
2018-11-26 12:17:41 +0200

    admin area not yet done

---
+++ admin/functions/add_article.php
@@ -0,0 +1,47 @@
+<?php 
+	include("../../functions/dbconnect.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Add Article</title>
+	<link rel="stylesheet" type="text/css" href="../../css/adminstyle.css">
+	<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">-->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+</head>
+<body>
+	<div class="container">
+		<div class ="head">
+			<img src="../../img/header.jpg" id="logo">
+		</div>
+		
+		<div class="barmenu">
+			<ul id="menu">
+				<a href="../index.php">Home Page</a>
+				<a href="add_article.php">Add article</a>
+				<a href="edit_post.php">Edit article</a>
+				<a href="add_category.php">Add category</a>
+				<a href="edit_comments.php">Edit comments</a>					
+			</ul>
+
+			<div id="form">
+				<form method="get" action="result.php" enctype="multipart/form-data">
+				<input style="border-radius: 10px;"type="text" name="searchquery" placeholder="search article">
+				<input style="border-radius: 10px; background-color: #e8e8e8;" type="submit" name="search" value="Search">
+			</form>
+			</div>	
+		</div>	
+
+		<div class="postarea">
+			<h1 align="center" style="color: #1a9e5a; font-family:MV Boli"> Add new article</h1>
+			<h2 style="color: #3a3a3a; padding-left: 45px; font-family: Calibri">Admin:</h2>
+			<?php include ("../../functions/insert_post.php") ?>
+		</div>
+		<div class="foterarea">
+			<p align="center"> 2018 &copy; <a href="../index.php">designed by kerrimov</a> </p>
+		</div>
+	</div>
+	
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,46 @@
+<?php 
+	include("../functions/dbconnect.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin area</title>
+	<link rel="stylesheet" type="text/css" href="../css/adminstyle.css">
+	<!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">-->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+</head>
+<body>
+	<div class="container">
+		<div class ="head">
+			<img src="../img/header.jpg" id="logo">
+		</div>
+		
+		<div class="barmenu">
+			<ul id="menu">
+				<a href="index.php">Home Page</a>
+				<a href="functions/add_article.php">Add article</a>
+				<a href="edit_post.php">Edit article</a>
+				<a href="add_category.php">Add category</a>
+				<a href="edit_comments.php">Edit comments</a>					
+			</ul>
+
+			<div id="form">
+				<form method="get" action="result.php" enctype="multipart/form-data">
+				<input style="border-radius: 10px;"type="text" name="searchquery" placeholder="search article">
+				<input style="border-radius: 10px; background-color: #e8e8e8;" type="submit" name="search" value="Search">
+			</form>
+			</div>	
+		</div>	
+
+		<div class="postarea">
+			<h1 align="center" style="color: #1a9e5a; font-family:MV Boli">Welcome admin area!</h1>
+			<h2 style="color: #3a3a3a; padding-left: 45px; font-family: Calibri">Admin:</h2>
+		</div>
+		<div class="foterarea">
+			<?php include ("../includes/footer.php")?>
+		</div>
+	</div>
+	
+</body>
+</html>
